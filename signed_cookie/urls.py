fa0991ec09af246c8bc3cb2c3af81d26af72fe13
Able <munirammar0@gmail.com>
2023-07-09 14:45:43 +0500

    Adding delete cookie functions

--- signed_cookie/urls.py
+++ signed_cookie/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('set/',views.set_signed_cookie,name='set_signed'),
-    path('get/',views.get_signed_cookie,name='get_signed')
+    path('get/',views.get_signed_cookie,name='get_signed'),
+    path('delete/',views.delete_signed_cookie,name='delete_signed')
 ]
--- signed_cookie/views.py
+++ signed_cookie/views.py
@@ -8,3 +8,8 @@ def set_signed_cookie(request):
 def get_signed_cookie(request):
     name = request.get_signed_cookie('name',salt='Am',default='Not Found')  # salt is the signature to identify the cookie
     return render(request,'signed_cookie/get.html',{'name':name})
+
+def delete_signed_cookie(request):
+    res = render(request,'signed_cookie/delete.html')
+    res.delete_cookie('name')
+    return res
